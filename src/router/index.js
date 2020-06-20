71db270880013c64729a712e8de71e9b06272c29
xiaoqiang-zhao <longze_xq@163.com>
2020-06-21 01:48:08 +0800

    移除 ts 支持。重新初始化项目

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
-import { RouteConfig, createRouter, createWebHistory } from 'vue-router';
+import { createRouter, createWebHistory } from 'vue-router';
 import Home from '../views/Home.vue'
 
-const routes: Array<RouteConfig> = [
+const routes = [
 {
   path: '/',
   name: 'Home',
