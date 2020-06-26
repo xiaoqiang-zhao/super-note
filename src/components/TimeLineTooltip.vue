cdf428b9a756145717b157dc7344c7c5a70b296a
xiaoqiang-zhao <longze_xq@163.com>
2020-06-26 19:29:41 +0800

    添加 tooltip 组件样式

--- src/components/TimeLineTooltip.vue
+++ src/components/TimeLineTooltip.vue
@@ -16,6 +16,7 @@ export default {
     // Before Christ
     const bc = this.time < 0 ? '前' : '';
     if (this.time === 0) {
+      // year of our lord 看着别扭，不和他们 our
       tooltipStr = 'year of lord';
     }
     else if (this.time % this.step === 0) {
@@ -34,8 +35,18 @@ export default {
 <style scoped lang="less">
 .time-line-tooltip {
   position: absolute;
+  top: 3px;
   left: 30px;
   display: none;
   color: #000;
+  font-size: 12px;
+  white-space: nowrap;
+}
+.start,
+.end {
+  .time-line-tooltip {
+    top: -1px;
+    left: 20px;
+  }
 }
 </style>
