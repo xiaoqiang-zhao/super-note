64253724f3ab9c15e4dd47daf5bffe8ebe1d32a1
xiaoqiang-zhao <longze_xq@163.com>
2020-07-09 14:54:07 +0800

    添加标志性历史人物

--- src/data/Persions/index.js
+++ src/data/Persions/index.js
@@ -4,6 +4,24 @@
 
 export default [
   {
+    name: '荷马',
+    bornIn: -873,
+    diedIn: -798,
+    portrait: '/static/Homer.jpeg'
+  },
+  {
+    name: '老子',
+    bornIn: -571,
+    diedIn: -471,
+    portrait: '/static/laozi.jpeg'
+  },
+  {
+    name: '释迦牟尼',
+    bornIn: -563,
+    diedIn: -483,
+    portrait: '/static/Buddha_meditating.jpeg'
+  },
+  {
     name: '孔子',
     bornIn: -551,
     diedIn: -479,
@@ -32,5 +50,113 @@ export default [
     bornIn: -250,
     diedIn: -150,
     portrait: '/static/mengzi.png'
-  }
+  },
+  {
+    name: '凯撒大帝',
+    bornIn: -100,
+    diedIn: -44,
+    portrait: '/static/kaisa.jpg'
+  },
+  {
+    name: '耶稣',
+    bornIn: -4,
+    diedIn: 33,
+    portrait: '/static/Christ.jpeg'
+  },
+  {
+    name: '蔡伦',
+    bornIn: 50,
+    diedIn: 121,
+    portrait: '/static/cailun.jpeg'
+  },
+  {
+    name: '陶渊明',
+    bornIn: 352,
+    diedIn: 427,
+    portrait: '/static/taoyuanming.jpg'
+  },
+  {
+    name: '穆罕默德',
+    bornIn: 571,
+    diedIn: 632,
+    portrait: '/static/mohammed.jpeg'
+  },
+  {
+    name: '李白',
+    bornIn: 701,
+    diedIn: 762,
+    portrait: '/static/Libai.jpeg'
+  },
+  {
+    name: '杜甫',
+    bornIn: 712,
+    diedIn: 770,
+    portrait: '/static/dufu.jpeg'
+  },
+  {
+    name: '王安石',
+    bornIn: 1021,
+    diedIn: 1086,
+    portrait: '/static/wanganshi.jpg'
+  },
+  {
+    name: '李清照',
+    bornIn: 1084,
+    diedIn: 1155,
+    portrait: '/static/liqingzhao.jpg'
+  },
+  {
+    name: '但丁',
+    bornIn: 1265,
+    diedIn: 1321,
+    portrait: '/static/Dante.jpg'
+  },
+  {
+    name: '薄伽丘',
+    bornIn: 1313,
+    diedIn: 1375,
+    portrait: '/static/bojiaqiu.png'
+  },
+  {
+    name: '古腾堡',
+    bornIn: 1398,
+    diedIn: 1468,
+    portrait: '/static/Gutenberg.jpeg'
+  },
+  {
+    name: '哥伦布',
+    bornIn: 1451,
+    diedIn: 1506,
+    portrait: '/static/Columbus.jpeg'
+  },
+  {
+    name: '米开朗基罗',
+    bornIn: 1475,
+    diedIn: 1564,
+    portrait: '/static/Volterra.jpeg'
+  },
+  {
+    name: '莎士比亚',
+    bornIn: 1564,
+    diedIn: 1661,
+    portrait: '/static/Shakespeare.jpeg'
+  },
+  {
+    name: '牛顿',
+    bornIn: 1643,
+    diedIn: 1727,
+    portrait: '/static/IsaacNewton.jpeg'
+  },
+  {
+    name: '贝多芬',
+    bornIn: 1770,
+    diedIn: 1827,
+    portrait: '/static/Beethoven.jpg'
+  },
+  {
+    name: '亚当·斯密',
+    bornIn: 1723,
+    diedIn: 1790,
+    portrait: '/static/AdamSmith.jpeg'
+  },
 ];
