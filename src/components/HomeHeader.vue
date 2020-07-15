e08e9ec1670fd5b13f23619ac64f9afb9b3476fc
xiaoqiang-zhao <longze_xq@163.com>
2020-07-15 18:55:13 +0800

    添加 HomeHeader 组件

---
+++ src/components/HomeHeader.vue
@@ -0,0 +1,59 @@
+<template>
+<header class="home-header">
+    <section
+      v-for="item in data.items"
+      :key="item.name"
+      :style="{'flex': `0 0 ${item.width}px`}"
+      class="item">
+      <template v-if="item.children">
+        <div class="title">{{item.name}}</div>
+        <section class="items">
+          <div class="item" v-for="child in item.children" :key="child.name">
+            {{child.name}}
+          </div>
+        </section>
+      </template>
+      <template v-else>
+        {{item.name}}
+      </template>
+    </section>
+</header>
+</template>
+
+<script>
+export default {
+  props: ['data'],
+  computed: {
+
+  },
+  methods: {
+
+  }
+}
+</script>
+
+<style lang="less" scoped>
+// 说明: 外层空间为纵向布局
+.home-header {
+  @height: 46px;
+  display: flex;
+  flex: 0 0 @height;
+  color: whitesmoke;
+  text-align: center;
+  background: yellowgreen;
+  .item {
+    line-height: @height;
+    .title,
+    .items {
+      line-height: @height / 2;
+    }
+    .items {
+      display: flex;
+      .item {
+        line-height: @height / 2;
+        flex: 1;
+      }
+    }
+  }
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,114 +1,82 @@
 <template>
-  <div class="home">
-    <HeaderBox :scale="scale" @changeScale="changeScale"/>
-    <section class="body-section">
-      <TimeLine class="time-line" :scale="scale" :space="20"/>
-      <Dynasty class="dynasty" :dynastyData="chinaDynasty" :scale="scale"/>
-      <Dynasty class="dynasty" :dynastyData="europeDynasty" :scale="scale" :colors="colors"/>
-      <section class="everything-container">
-        <section class="container-item persions-container" :style="persionsContainerStyle">
-          <Persion v-for="item in persions" :key="item.name" :data="item" :scale="scale"/>
-        </section>
-        <section class="container-item books-container" :style="persionsContainerStyle">
-          <Book v-for="item in books" :key="item.name" :data="item" :scale="scale"/>
-        </section>
-      </section>
+  <section class="home">
+    <!-- <TimeLine class="time-line"/> -->
+    <aside class="left" :style="{'flex': `0 0 ${header.left.width}px`}">
+      <HomeHeader :data="header.left"/>
+    </aside>
+    <section class="middle" :style="{'flex': `0 0 ${header.middle.width}px`}">
+      <HomeHeader :data="header.middle"/>
     </section>
-  </div>
+    <aside class="right" :style="{'flex': `0 0 ${header.right.width}px`}">
+      <HomeHeader :data="header.right"/>
+    </aside>
+  </section>
 </template>
 
 <script>
 /**
  * @file 主页
  */
-import HeaderBox from '@/components/HeaderBox'
-import TimeLine from '@/components/TimeLine'
-import Dynasty from '@/components/Dynasty'
-import Persion from '@/components/Persion'
-import Book from '@/components/Book'
-
-import chinaDynasty from '@/components/Dynasty/china';
-import europeDynasty from '@/components/Dynasty/europe';
-import persions from '@/data/Persions';
-import books from '@/data/Books';
+import HomeHeader from '@/components/HomeHeader'
+// import TimeLine from '@/components/TimeLine'
 
 export default {
   name: 'home',
   components: {
-    HeaderBox,
-    TimeLine,
-    Dynasty,
-    Persion,
-    Book
+    HomeHeader
+    // TimeLine,
   },
   data() {
     return {
-      chinaDynasty,
-      europeDynasty,
-      colors: [
-        '#27309A',
-        '#E97F02',
-        '#8CD790',
-        '#30A9DE'
-      ],
-      persions,
-      books,
-      persionsContainerStyle: {}
+      header: this.$store.state.header
     }
   },
-  computed: {
-    // 缩放率
-    scale() {
-      return this.$store.state.scale
-    },
-    persionColumnMax() {
-      return this.$store.state.persionColumnMax;
-    }
-  },
-  watch: {
-    persionColumnMax() {
-      this.persionsContainerStyle = {
-        flex: `0 0 ${(this.persionColumnMax + 1) * 110}px`
-      }
-    }
-  },
-  methods: {
-
-    /**
-     * 改变比例尺，时间与页面尺寸的比例在 1:1 和 1:10 之间切换
-     */
-    changeScale(value) {
-      this.scale = value;
-    }
-  }
+  methods: {}
 }
 </script>
 
 <style lang="less" scoped>
-.home .body-section {
+.home {
   display: flex;
-  margin: 0 10px 100px;
-  .time-line {
-    flex: 0 0 50px;
-    margin-right: 41px;
-  }
-  .dynasty {
-    flex: 0 0 81px;
-    margin: 0 5px;
-  }
-  .everything-container {
+
+  .left,
+  .right,
+  .middle {
+    @header-height: 30px;
     display: flex;
-    margin-left: 5px;
-    .container-item {
-      position: relative;
-      height: 100%;
-      border-right: dashed 1px #6C49B8;
+    flex-direction: column;
+    // > header {
+    //   flex: 0 0 @header-height;
+    //   line-height: @header-height;
+    //   margin: 5px;
+    //   overflow: hidden;
+    //   background: #4F86C6;
+    //   text-align: center;
+    // }
+    > section {
+      flex: 1;
+      margin: 5px;
+      padding-bottom: 5px;
+      overflow: hidden;
+      background: #4FB0C6;
     }
-    .persions-container {
-      border-color: #6C49B8;
-    }
-    .books-container {
-      border-color: #4F86C6;
+  }
+
+  .left,
+  .right {
+    flex: 0 0 100px;
+  }
+  .middle {
+    flex: 1;
+    background: whitesmoke;
+    > section {
+      max-width: 590px;
+      > div {
+        display: flex;
+        .content-item {
+          margin-bottom: 0;
+        }
+      }
     }
   }
 }
