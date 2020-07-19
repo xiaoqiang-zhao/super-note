a1bb39057a294f00edaa7f0559701eaa9904e9a6
xiaoqiang-zhao <longze_xq@163.com>
2020-07-19 21:43:19 +0800

    添加数据 古腾堡印刷机

--- src/data/Technology/index.js
+++ src/data/Technology/index.js
@@ -7,5 +7,10 @@ export default [
     name: '雕版印刷术',
     time: 636,
     cover: '/static/technology-cover/雕版印刷术.jpg'
+  },
+  {
+    name: '古腾堡印刷机',
+    time: 1439,
+    cover: '/static/technology-cover/古腾堡印刷机.jpg'
   }
 ];
