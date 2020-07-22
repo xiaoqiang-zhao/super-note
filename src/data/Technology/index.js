7afa3181ed0ff3bfee61d1996c30531a2e12c062
xiaoqiang-zhao <longze_xq@163.com>
2020-07-22 22:10:37 +0800

    添加数据 管装颜料

--- src/data/Technology/index.js
+++ src/data/Technology/index.js
@@ -16,6 +16,13 @@ export default [
   {
     name: '照相机',
     time: 1839,
-    cover: '/static/technology-cover/照相机.png'
+    cover: '/static/technology-cover/照相机.png',
+    pro: '1839年8月19日法国画家路易·达盖尔发明了世界上第一台真正的照相机，可携式木箱照相机'
+  },
+  {
+    name: '管装颜料',
+    time: 1841,
+    cover: '/static/technology-cover/管装颜料.png',
+    pro: '此前画家离开工作室代价巨大，1841年美国人约翰・戈夫・兰德在美国为这一发明申请了专利'
   }
 ];
